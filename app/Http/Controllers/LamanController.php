b5ba8717a254cbe5c89fda749dbe48987701de66
Ananda <qq.kurniadi445@gmail.com>
2022-10-14 23:07:06 +0700

    Tambah controller laman (transaksi)

--- app/Http/Controllers/LamanController.php
+++ app/Http/Controllers/LamanController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class LamanController extends Controller
 {
+    public function transaksi(): Factory|View|Application
+    {
+        return view('tampilan.laman.transaksi');
+    }
+
+    public function prosesTransaksi(Request $request): Factory|View|Application
+    {
+        $keterlambatan = $request->input('keterlambatan');
+        $denda = $request->input('denda');
+
+        $totalDenda = (new Transaksi())->hitungDenda($keterlambatan, $denda);
+
+        return view('tampilan.laman.transaksi', [
+            'total_denda' => $totalDenda
+        ]);
+    }
+
     public function tentang(): Factory|View|Application
     {
         return view('tampilan.laman.tentang');
